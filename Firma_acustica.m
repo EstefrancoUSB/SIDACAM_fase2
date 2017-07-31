1fe9edd79dad18f8b6e87515bc0af35c5c8b0015
estebang90 <estebang90@gmail.com>
2017-07-31 10:06:12 -0500

    Base de datos desde la función
    1- Se modifica Firma_acustica.m para que se haga la lectura de la base de datos desde la función.
    2- La parametrización (aumento del contraste) se hace por fuera del ciclo FOR, para optimizar tiempos.

--- Firma_acustica.m
+++ Firma_acustica.m
@@ -1,6 +1,6 @@
 function [Max_Bandas_dB, Frec_Max,Frecuencias,Comparacion_Prom,Firma,Comparacion_Log]...
     = Firma_acustica(code, Dim_fft, Frec_Corte1, N_Frec, Step)
-            
+
 %FIRMA ACUSTICA
 % ---------------------------------------------------------------
 % Luis Alberto Tafur Jimenez, decano.ingenierias@usbmed.edu.co
@@ -14,7 +14,7 @@ function [Max_Bandas_dB, Frec_Max,Frecuencias,Comparacion_Prom,Firma,Comparacion
 
 %ENTRADAS
 %code           String, contiene el codigo de la embarcacion
-%Dim_fft        double, 
+%Dim_fft        double,
 %Frec_Corte1
 %N_Frec
 %Step
@@ -28,18 +28,33 @@ function [Max_Bandas_dB, Frec_Max,Frecuencias,Comparacion_Prom,Firma,Comparacion
 %%Inicializacion de variables
 Orden_Filtro = 8;
 Max_Bandas_dB = zeros(1,N_Frec);
-Comparacion_Prom = zeros(1,N_Frec);
-Comparacion_Log = zeros(1,N_Frec);
+%Comparacion_Prom = zeros(1,N_Frec);
+%Comparacion_Log = zeros(1,N_Frec);
 Promedio = sum(Max_Bandas_dB)/N_Frec;
+load info_barcos
+%% posicionamiento
+%Analiza en la database si el codigo de la embarcacion ya
+            %existe, de lo contrario la almacena
+            posicion = find(strcmp(info_barcos{1,1}, code));
+if posicion ~= 0
+    fprintf('La embarcacion ya existe en la posicion %s \n', num2str(posicion))
+    info_barcos{3,1}(posicion) = info_barcos{3,1}(posicion) + 1;
+    fprintf('Numero de grabaciones: %i \n', info_barcos{3,1}(posicion));
+else
+    posicion = find(strcmp(info_barcos{1,1}, '0'),1);
+    info_barcos{1,1}{posicion} = code;
+    info_barcos{3,1}(posicion) = info_barcos{3,1}(posicion) + 1;
+    fprintf('Numero de grabaciones: %i \n', info_barcos{3,1}(posicion));
+end
 
-code = [code '.wav']; %Concatena el codigo de la embarcacion con la extension .wav
-[S_Blanco, Frec_Muestreo] = audioread(code); %Lee la señal de la embarcacion y extrae su
-                                             %frecuencia de muestreo
-       
+audio = [code '.wav']; %Concatena el codigo de la embarcacion con la extension .wav
+[S_Blanco, Frec_Muestreo] = audioread(audio); %Lee la señal de la embarcacion y extrae su
+%frecuencia de muestreo
+
 for ContMaximo=1:N_Frec
     
     % Diseno Filtro Pasa-Banda
-       Frec_Corte2 = Frec_Corte1 + Step;       % Donde el ancho de banda = Step
+    Frec_Corte2 = Frec_Corte1 + Step;       % Donde el ancho de banda = Step
     Param_Filtro = fdesign.bandpass('N,F3dB1,F3dB2',...
         Orden_Filtro,Frec_Corte1,Frec_Corte2,Frec_Muestreo);
     Filtro = design(Param_Filtro,'butter');
@@ -53,13 +68,19 @@ for ContMaximo=1:N_Frec
     pxxdB = 10*log10(pxx);
     
     % Busqueda de los valores Maximos y su correspondiente frecuencia
-    [Max_Bandas_dB(ContMaximo),posicion] = max(pxxdB);
-    Frec_Max(ContMaximo) = Frecuencias(posicion);
+    [Max_Bandas_dB(ContMaximo),pos] = max(pxxdB);
+    Frec_Max(ContMaximo) = Frecuencias(pos);
     Frec_Corte1 = Frec_Corte2;
     
-    % Parametrizacion de la firma acustica
-    Comparacion_Prom(ContMaximo) = Max_Bandas_dB(ContMaximo)/Promedio;          
-    Firma(ContMaximo) =1./(Comparacion_Prom(ContMaximo))^100;    
-    Comparacion_Log(ContMaximo) = log10(Comparacion_Prom(ContMaximo)); 
 end
+
+ % Parametrizacion de la firma acustica
+    Comparacion_Prom = Max_Bandas_dB/Promedio;
+    Firma =1./(Comparacion_Prom).^100;
+    Comparacion_Log = log10(Comparacion_Prom);
+    
+    info_barcos{2,1}{posicion}(1:2,1:35,info_barcos{3,1}(posicion)) =...
+    [Max_Bandas_dB;Frec_Max];
+save('info_barcos','info_barcos')
+
 
