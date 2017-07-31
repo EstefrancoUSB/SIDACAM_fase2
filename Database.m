73bc0675edd75117270db3f79c2052154a262ea6
estebang90 <estebang90@gmail.com>
2017-07-31 10:11:17 -0500

    Megamatriz
    1- Se crea y se introduce la megamatriz en la Database

--- Database.m
+++ Database.m
@@ -1,12 +1,18 @@
 %% Inicializacion de la base de datos.
 
- codigo_barco = cell(1,20);
- max_y_frec = cell(1,20);
- N_grabaciones = zeros(1,20);
- frec_principales = cell(1,20);
- info_barcos = {codigo_barco; max_y_frec; N_grabaciones; frec_principales};
- 
+
+
+codigo_barco = cell(1,20);
+max_y_frec = cell(1,20);
+N_grabaciones = zeros(1,20);
+frec_principales = cell(1,20);
+info_barcos = {codigo_barco; max_y_frec; N_grabaciones; frec_principales};
+
 cambiar = cellfun('isempty',info_barcos{1,1});
 info_barcos{1,1}(cambiar) = {'0'};
-           
-save('info_barcos')
+
+for barco = 1:length(info_barcos{1,1})
+     info_barcos{2,1}{barco} = zeros(2,35,100);
+end
+
+save('info_barcos','info_barcos')
