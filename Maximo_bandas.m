6439898fc4a151a1e6f24b06276e90c4de21f2e2
Davidpz93 <b_hh@hotmail.es>
2017-10-03 22:53:13 -0500

    Add files via upload
    Tiene algunas modificaciones a la hora de leer el archivo de audio que se ingresará para agregar una firma acústica a la base de datos. Por ej. ya divide la opción de entrada en leer el archivo directamente, agregar otra carpeta o grabarlo.

--- Maximo_bandas.m
+++ Maximo_bandas.m
@@ -1,4 +1,5 @@
-function [Max_Bandas_dB, Frec_Max]= Maximo_bandas(code, Dim_fft, Frec_Corte1, N_Frec, Step)
+function [posicion, Max_Bandas_dB, Frec_Max]= Maximo_bandas(code, Dim_fft, Frec_Corte1, ...
+    N_Frec, Step, varargin)
 
 %MAXIMO BANDAS
 % ---------------------------------------------------------------
@@ -24,35 +25,53 @@ function [Max_Bandas_dB, Frec_Max]= Maximo_bandas(code, Dim_fft, Frec_Corte1, N_
 %Firma,Comparacion_Log
 
 load info_barcos
-flag = 1;
 %% posicionamiento
-%Analiza en la database si el codigo de la embarcacion ya
-%existe, de lo contrario la almacena
+%Analiza en la database si el código de la embarcación ya
+%existe, de lo contrario la almacena.
 
 
-while flag == 1
+    
+    %% Posicionamiento de la data en la base de datos info_barcos
+    
+    %Encontrando si existe la embarcación ingresada en code.
     posicion = find(strcmp(info_barcos{1,1}, code));
     
     if posicion ~= 0
-        fprintf('La embarcacion ya existe en la posicion %d \n', posicion)
+        %Se encuentra la embarcación y se pone en la celda info_barcos{3,1}(posicion)
+        fprintf('La embarcación ya existe en la posición %d \n', posicion)
         info_barcos{3,1}(posicion) = info_barcos{3,1}(posicion) + 1;
-        fprintf('Numero de grabaciones: %i \n', info_barcos{3,1}(posicion));
+        fprintf('Número de grabaciones: %i \n', info_barcos{3,1}(posicion));
     else
+        %Si no se encuentra se introduce la nueva embarcación. 
+        fprintf('Se introdujo una nueva embarcación: %s \n', code);
         posicion = find(strcmp(info_barcos{1,1}, '0'),1);
         info_barcos{1,1}{posicion} = code;
         info_barcos{3,1}(posicion) = info_barcos{3,1}(posicion) + 1;
-        fprintf('Numero de grabaciones: %i \n', info_barcos{3,1}(posicion));
+        fprintf('Número de grabaciones: %i \n', info_barcos{3,1}(posicion));
     end
     
-    
-    audio = [code '.wav']; %Concatena el codigo de la embarcacion con la extension .wav
-    [S_Blanco, Frec_Muestreo] = audioread(audio); %Lee la señal de la embarcacion y extrae su
-    %frecuencia de muestreo
-    
+    %Condicionantes para saber si el audio se lee desde la carpeta de
+    %trabajo, otra carpeta externa o una grabación.
+    if nargin >=6
+       if nargin==6
+            audio=[varargin{1} '.wav']; %Lee el nombre del archivo
+            [S_Blanco, Frec_Muestreo] = audioread(audio);
+            
+        elseif nargin==7
+                addpath(varargin{2}); %Añade el path para búsqueda.
+                audio=[varargin{1} '.wav'];
+                [S_Blanco, Frec_Muestreo] = audioread(audio);
+       elseif nargin == 8
+           S_Blanco = varargin{3};
+           Frec_Muestreo = 48000;
+        end
+    end
+         
+        %Extrayendo máximos de frecuencia específicos por anchos de banda.
     [Max_Bandas_dB,Frec_Max] = Banco_filtros (S_Blanco,Frec_Muestreo, N_Frec,Dim_fft,...
         Frec_Corte1, Step);
     
-    
+    %Depurando grabaciones mal hechas.
     Pregunta = input('¿Desea grabar éste recorrido? SI/NO: ', 's');
     
     if strcmp(Pregunta, 'NO')== 1;
@@ -63,17 +82,8 @@ while flag == 1
         info_barcos{2,1}{posicion}(1:2,1:35,info_barcos{3,1}(posicion)) =...
             [Max_Bandas_dB;Frec_Max];
     end
-    
-    
-    %Pregunta sobre seguir grabando más firmas acústicas.
-    Mas_firmas = input('¿Desea grabar más recorridos de esta lancha? SI/NO: ', 's');
-    
-    if strcmp(Mas_firmas, 'NO')== 1
-        flag = 2;
-        disp('__________')
-    end
-end
+  
+
 save('info_barcos','info_barcos')
 
 %Llama a la función firma acústica.
-Firma_acustica (posicion,N_Frec)
